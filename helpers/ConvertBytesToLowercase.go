c65279a6fc5becdf99834b18c75c06f8e468acc9
Danny Zolp <dzolp@roadamerica.com>
2025-10-06 22:21:48 -0500

    +1

--- helpers/ConvertBytesToLowercase.go
+++ helpers/ConvertBytesToLowercase.go
@@ -1,22 +1,15 @@
 package helpers
 
 func ConvertBytesToLowercase(bytes []byte) []byte {
-	i := 0
-	for i < len(bytes) {
-		length := int(bytes[i])
+	length := 0
+	for i := 0; i < len(bytes); i++ {
 		if length == 0 {
-			i++
-			break
-		}
-		i++
-		if i+length > len(bytes) {
-			break // invalid length
-		}
-		for i <= length {
-			if bytes[i] <= 90 && bytes[i] >= 65 {
+			length = int(bytes[i])
+		} else {
+			if bytes[i] >= 65 && bytes[i] <= 90 {
 				bytes[i] += 32
 			}
-			i++
+			length--
 		}
 	}
 	return bytes
